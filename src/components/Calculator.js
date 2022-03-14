c9ca71020bc63686acb8860e674509bbefb3a160
Estebanmual <esteban.munozal@gmail.com>
2022-03-14 12:36:44 -0600

    On branch feature/components 	deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Calculator.js 	new file:   src/components/Calculator.module.css 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	Added the calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './Calculator.module.css';
+
+function Calculator() {
+  return (
+    <table className={styles.calculator} id="calculator__table">
+      <tbody>
+        <tr>
+          <td className={styles.calculator__result} colSpan="4">0</td>
+        </tr>
+        <tr className={styles.calculator__button}>
+          <td><button type="button">AC</button></td>
+          <td><button type="button">+/-</button></td>
+          <td><button type="button">%</button></td>
+          <td className={styles.calculator__button__orange}><button type="button">÷</button></td>
+        </tr>
+        <tr>
+          <td><button type="button">7</button></td>
+          <td><button type="button">8</button></td>
+          <td><button type="button">9</button></td>
+          <td className={styles.calculator__button__orange}><button type="button">x</button></td>
+        </tr>
+        <tr>
+          <td><button type="button">4</button></td>
+          <td><button type="button">5</button></td>
+          <td><button type="button">6</button></td>
+          <td className={styles.calculator__button__orange}><button type="button">-</button></td>
+        </tr>
+        <tr>
+          <td><button type="button">1</button></td>
+          <td><button type="button">2</button></td>
+          <td><button type="button">3</button></td>
+          <td className={styles.calculator__button__orange}><button type="button">+</button></td>
+        </tr>
+        <tr>
+          <td colSpan="2"><button type="button">0</button></td>
+          <td><button type="button">.</button></td>
+          <td className={styles.calculator__button__orange}><button type="button">=</button></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Calculator;
