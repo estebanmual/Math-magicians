467f25b6e98b123ab4999af092f4ef35be9bbda5
Estebanmual <esteban.munozal@gmail.com>
2022-03-14 14:01:33 -0600

    Change functional based component to class based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import { Component } from 'react';
 import Calculator from './components/Calculator';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
-
-export default App;
