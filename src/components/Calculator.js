1ed84e96dd8d37670ac4580de80ee2384da97151
Estebanmual <esteban.munozal@gmail.com>
2022-03-14 18:33:47 -0600

    Refactor from class based component to functional based component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,67 +1,59 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './Calculator.module.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.clickHandler = this.clickHandler.bind(this);
+export default function Calculator() {
+  const [total, setTotal] = useState(0);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+
+  function clickHandler(e) {
+    const result = calculate({ total, next, operation }, e.target.textContent);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
   }
 
-  clickHandler(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <table className={styles.calculator} id="calculator__table">
-        <tbody>
-          <tr>
-            <td className={styles.calculator__result} colSpan="4">
-              { total }
-              { operation }
-              { next }
-            </td>
-          </tr>
-          <tr className={styles.calculator__button}>
-            <td><button type="button" onClick={this.clickHandler}>AC</button></td>
-            <td><button type="button" onClick={this.clickHandler}>+/-</button></td>
-            <td><button type="button" onClick={this.clickHandler}>%</button></td>
-            <td className={styles.calculator__button__orange}><button type="button" onClick={this.clickHandler}>÷</button></td>
-          </tr>
-          <tr>
-            <td><button type="button" onClick={this.clickHandler}>7</button></td>
-            <td><button type="button" onClick={this.clickHandler}>8</button></td>
-            <td><button type="button" onClick={this.clickHandler}>9</button></td>
-            <td className={styles.calculator__button__orange}><button type="button" onClick={this.clickHandler}>x</button></td>
-          </tr>
-          <tr>
-            <td><button type="button" onClick={this.clickHandler}>4</button></td>
-            <td><button type="button" onClick={this.clickHandler}>5</button></td>
-            <td><button type="button" onClick={this.clickHandler}>6</button></td>
-            <td className={styles.calculator__button__orange}><button type="button" onClick={this.clickHandler}>-</button></td>
-          </tr>
-          <tr>
-            <td><button type="button" onClick={this.clickHandler}>1</button></td>
-            <td><button type="button" onClick={this.clickHandler}>2</button></td>
-            <td><button type="button" onClick={this.clickHandler}>3</button></td>
-            <td className={styles.calculator__button__orange}><button type="button" onClick={this.clickHandler}>+</button></td>
-          </tr>
-          <tr>
-            <td colSpan="2"><button type="button" onClick={this.clickHandler}>0</button></td>
-            <td><button type="button" onClick={this.clickHandler}>.</button></td>
-            <td className={styles.calculator__button__orange}><button type="button" onClick={this.clickHandler}>=</button></td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+  return (
+    <table className={styles.calculator} id="calculator__table">
+      <tbody>
+        <tr>
+          <td className={styles.calculator__result} colSpan="4">
+            { total }
+            { operation }
+            { next }
+          </td>
+        </tr>
+        <tr className={styles.calculator__button}>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>AC</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>+/-</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>%</button></td>
+          <td className={styles.calculator__button__orange}><button type="button" onClick={clickHandler}>÷</button></td>
+        </tr>
+        <tr>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>7</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>8</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>9</button></td>
+          <td className={styles.calculator__button__orange}><button type="button" onClick={clickHandler}>x</button></td>
+        </tr>
+        <tr>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>4</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>5</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>6</button></td>
+          <td className={styles.calculator__button__orange}><button type="button" onClick={clickHandler}>-</button></td>
+        </tr>
+        <tr>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>1</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>2</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>3</button></td>
+          <td className={styles.calculator__button__orange}><button type="button" onClick={clickHandler}>+</button></td>
+        </tr>
+        <tr>
+          <td colSpan="2" className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>0</button></td>
+          <td className={styles.calculator__button__grey}><button type="button" onClick={clickHandler}>.</button></td>
+          <td className={styles.calculator__button__orange}><button type="button" onClick={clickHandler}>=</button></td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
-
-export default Calculator;
